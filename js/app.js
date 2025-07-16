fa520442e26f7251a0bd20272cd25ca830435864
lucas-dias7 <w7.lucasdias@gmail.com>
2025-07-16 14:23:48 -0300

    declarando funcoes

---
+++ js/app.js
@@ -0,0 +1,31 @@
+function adicionar() {
+ // recuperar valores: nome do produto, qtd e valor.
+let nomeProduto = document.getElementById('produto');
+let quantidade = document.getElementById('quantidade');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ // calcular o preço, o nosso subtotal
+ // adicionar no carrinho
+ // atualizar o valor total
+
+}
+
+
+
+
+function limpar() {
+ 
+}
